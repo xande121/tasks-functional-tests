fdb32f7dee8893563c4146332e1dd60576e7e4eb
Alexandre Mantovan <ale98.lm@outlook.com>
2024-06-15 19:35:33 -0300

    fix: teste setar chrome driver manual

--- src/test/java/br/ce/amantovan/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/amantovan/tasks/functional/TasksTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TasksTest {
 
 	public WebDriver acessarAplicacao() {
+	    System.setProperty("webdriver.chrome.driver", "C:/Users/ale98/OneDrive/Desktop/dev/java/seleniumDrivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://localhost:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
